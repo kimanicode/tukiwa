9a0becd39f4cb52a304312134e8fc5b0e3ede8ca
kimanicode <karaba.dev@gmail.com>
2025-03-12 12:53:13 +0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    distDir: "dist", // Change build output folder from `.next` to `dist`
+    
   };
   
   export default nextConfig;
