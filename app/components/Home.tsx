84007656a2581efe7dc5597c1fd89caf22188d24
kimanicode <karaba.dev@gmail.com>
2025-03-12 13:43:12 +0300

    fixed homepage mobile view

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,28 +1,31 @@
-"use client";
-import { useRouter } from "next/navigation";
+"use client"
+import { useRouter } from 'next/navigation'
 
 const Home = () => {
-  const router = useRouter();
+  const router = useRouter()
+
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-white to-[#f3f4f6]">
-      <div className="flex flex-grow items-center justify-center text-center">
-        <div>
-          <h1 className="text-4xl md:text-6xl bg-gradient-to-r from-new-red to-periwinkle inline-block text-transparent bg-clip-text">
-            Fundraising Made Simple
-          </h1>
-          <p className="py-6 text-base-100 md:text-xl">
-            Let your Fundraiser run on <span className="">Autopilot</span>
-          </p>
-          <button
-            className="btn rounded-full bg-base-200 text-white font-light md:w-1/3 border-2 border-white py-5 hover:border-transition duration-300 hover:border-periwinkle"
-            onClick={() => router.push("/auth/log-in")}
-          >
-            Start a Fundraiser
-          </button>
+    <div>
+      <div className="hero bg-gradient-to-r from-white to-[#f3f4f6] min-h-[40vh] md:min-h-screen">
+        <div className="hero-content text-center">
+          <div className="">
+            <h1 className="text-4xl md:text-6xl bg-gradient-to-r from-new-red to-periwinkle inline-block text-transparent bg-clip-text">
+              Fundraising Made Simple
+            </h1>
+            <p className="py-6 text-base-100 md:text-xl">
+              Let your Fundraiser run on <span>Autopilot</span>
+            </p>
+            <button
+              className="btn rounded-full bg-base-200 text-white font-light md:w-1/3 border-2 border-white py-5 hover:border-transition duration-300 hover:border-periwinkle"
+              onClick={() => router.push("/auth/log-in")}
+            >
+              Start a Fundraiser
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
